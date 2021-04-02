7646c52c248c5e60e55c66fb9fb424f24630ef1c
Thomas Hansknecht <ghost24az@gmail.com>
2021-04-02 10:00:49 -0500

    I started making a log in interface

---
+++ log_in_Screen.java
@@ -0,0 +1,16 @@
+// Thomas Hansknecht
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Random;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+
+public class log_in_Screen extends JFrame{
+
+
+}
