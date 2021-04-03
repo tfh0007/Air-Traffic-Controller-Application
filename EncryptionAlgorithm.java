6f323966fcd0221733371c5e2332d228b49764af
Thomas H <tfh0007@auburn.edu>
2021-04-02 21:30:23 -0500

    Absolutely no progress has been made on creating an encryption algorithm

--- EncryptionAlgorithm.java
+++ EncryptionAlgorithm.java
@@ -2,10 +2,7 @@
 /* This class handles encoding and decoding user passwords
 */
 
-//External Sources: 
-//Learned how to convert a string into Binary with https://mkyong.com/java/java-convert-string-to-binary/
-//Learned how to XOR two binary string together in java with https://www.geeksforgeeks.org/xor-of-two-binary-strings/
-
+import java.util.Arrays;
 
 public class EncryptionAlgorithm {
 
@@ -15,7 +12,7 @@ public class EncryptionAlgorithm {
    
       String result;
    
-
+   
    
    
    
@@ -23,70 +20,29 @@ public class EncryptionAlgorithm {
    }
 
 
-   public static String EncodeRequest(String plainText, String key) {
+   public static String EncodeRequest(String plainText, String key)  {
    
-      int binaryPlainTextSize, binaryKeySize;
-         String binaryResult = "";
-         String result = "";
+
+         String result ="";
       
-
-   
-   
-      StringBuilder binaryPlainText = new StringBuilder();
-      StringBuilder binaryKeyText = new StringBuilder();
-   
-      StringBuilder binaryCipherText = new StringBuilder();
-   
-   
-      char[] charplainText = plainText.toCharArray();
-      char[] charkey = key.toCharArray();
-   
-      for (char aBit : charplainText) {
-         binaryPlainText.append(
-            String.format("%8s", Integer.toBinaryString(aBit))   
-            .replaceAll(" ", "0"));
-                    
-      }
-      System.out.println("DEBUG: After conversion to binary of text: " + plainText + " we get -> " + binaryPlainText);
-
-   
-      for (char aBit : charkey) {
-         binaryKeyText.append(
-            String.format("%8s", Integer.toBinaryString(aBit))   
-            .replaceAll(" ", "0"));
-                    
-      }
-      System.out.println("DEBUG: After conversion to binary of text: " + key + " we get -> " + binaryKeyText);
-       
-   
-   
-      binaryPlainTextSize = binaryPlainText.length();
+                 
    
 
-      for (int i = 0; i < binaryPlainTextSize; i++) {
+      System.out.println("DEBUG: After the Encryption process our cipher text is -> " + result);
       
-         if (binaryPlainText.charAt(i) == binaryKeyText.charAt(i)) {
-            binaryResult += "0";
-            
-         }
-         else {
-            binaryResult += "1";
          
-         }
-      }
       
       // Convert The binary char array into a string of hexadecimal values
       
       //Convert the binaryResult string builder into a string
-      result = binaryResult.toString();
+         
       
+   
       
-
-      
-
+   
         // System.out.println("DEBUG: After the Encryption process our cipher text is -> " + hexString);
        
-
+   
          
          
    
@@ -98,3 +54,4 @@ public class EncryptionAlgorithm {
 
 
 }
+
