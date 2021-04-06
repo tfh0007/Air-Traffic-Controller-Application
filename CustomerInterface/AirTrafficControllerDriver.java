9e39f2d6aa57497301e01f7e9feb9f46989ee56e
Thomas H <tfh0007@auburn.edu>
2021-04-06 16:05:11 -0500

    I am trying out the progress bar on lanuch

--- CustomerInterface/AirTrafficControllerDriver.java
+++ CustomerInterface/AirTrafficControllerDriver.java
@@ -2,15 +2,35 @@
 /* This class is the executable for running the AirTrafficController customer interface.
  * Simply run this class to run the interface
  */
-public class AirTrafficControllerDriver {
+public class AirTrafficControllerDriver extends Thread {
 
-
+   public void run() {
+      ProgressBar bar2 = new ProgressBar();
+      bar2.fillFast();
+      
+      }
 
    public static void main (String[] args) {
    
-   // Signal for the login interface to run  
-      log_in_Screen frame1;
-      frame1 = new log_in_Screen();
-         
+   // Signal for the login interface to run 
+//    
+//       AirTrafficControllerDriver object = new AirTrafficControllerDriver();
+//       object.start();
+      
+//       try {
+//       Thread.sleep(8500);
+//       }
+//       catch (InterruptedException f){
+//       }
+//       log_in_Screen frame1;
+//       frame1 = new log_in_Screen();
+
+           ProgressBar bar2 = new ProgressBar();
+           bar2.fillFast();
+     
+     
+           log_in_Screen frame1;
+           frame1 = new log_in_Screen();
+    
     }
 }
