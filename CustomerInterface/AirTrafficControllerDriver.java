e59db4ab8730934a510a31363740dd2d16863f49
Thomas H <tfh0007@auburn.edu>
2021-04-07 00:23:27 -0500

    I used a much better and easier to understand implementation for menu objects

--- CustomerInterface/AirTrafficControllerDriver.java
+++ CustomerInterface/AirTrafficControllerDriver.java
@@ -2,28 +2,10 @@
 /* This class is the executable for running the AirTrafficController customer interface.
  * Simply run this class to run the interface
  */
-public class AirTrafficControllerDriver extends Thread {
-   // Every thread after the first will get here
-   public void run() {
-      ProgressBar bar2 = new ProgressBar();
-      bar2.fillFast();
-      
-      }
+public class AirTrafficControllerDriver  {
 
    public static void main (String[] args) {
    
-   // Signal for the login interface to run 
-//    
-//       AirTrafficControllerDriver object = new AirTrafficControllerDriver();
-//       object.start();
-      
-//       try {
-//       Thread.sleep(8500);
-//       }
-//       catch (InterruptedException f){
-//       }
-//       log_in_Screen frame1;
-//       frame1 = new log_in_Screen();
 
            ProgressBar bar2 = new ProgressBar();
            bar2.fillFast();
